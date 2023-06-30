2eac3282a58e50d4391a585d5498e3ff2ca14d6c
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-06-30 11:50:56 +0530

    add external link icon

--- src/components/MadeBy.tsx
+++ src/components/MadeBy.tsx
@@ -1,9 +1,18 @@
+import ExternalLink from "./icons/ExternalLink";
+
 function MadeBy() {
   return (
-    <div className="absolute bottom-4 right-2 sm:right-8 text-white/40 text-xs sm:text-sm">
-      Made by{" "}
-      <a href="https://ajoykumardas.vercel.app/" className="font-semibold">
-        Ajoy
+    <div className="absolute bottom-4 right-0 xs:right-2 sm:right-6 text-white/40 hover:text-white/60 text-xs sm:text-sm group">
+      <a
+        href="https://ajoykumardas.vercel.app/"
+        target="_blank"
+        rel="noopener"
+        className="font-semibold flex items-center gap-1"
+      >
+        Made by Ajoy{" "}
+        <div className="opacity-0 group-hover:opacity-100 transition">
+          <ExternalLink />
+        </div>
       </a>
     </div>
   );
--- src/components/Tourist.tsx
+++ src/components/Tourist.tsx
@@ -1,6 +1,6 @@
 function Tourist() {
   return (
-    <div className="absolute -bottom-24 left-2/4 -translate-x-2/4">
+    <div className="absolute -bottom-20 lg:-bottom-24 left-2/4 -translate-x-2/4">
       <img
         src="/images/tourist.png"
         alt="tourist"
