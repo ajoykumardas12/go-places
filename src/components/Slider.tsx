f7d752ed8a103452b7fc9698c6096e4995670723
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-07-01 09:55:42 +0530

    update slider controls style

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -118,7 +118,7 @@ function Slider() {
       )}
       <button onClick={nextSlide} className="absolute bottom-2/4 right-2 group">
         <motion.div whileTap={{ scale: 0.4 }}>
-          <RightArrow iconClass="w-10 h-10 sm:w-14 sm:h-14 fill-gray-400 group-hover:fill-violet-700 opacity-50 group-hover:opacity-100" />
+          <RightArrow iconClass="w-10 h-10 sm:w-14 sm:h-14 fill-gray-300 group-hover:fill-purple-800 opacity-60 group-hover:opacity-100" />
         </motion.div>
       </button>
       <button
@@ -126,7 +126,7 @@ function Slider() {
         className="absolute bottom-2/4 left-2 group"
       >
         <motion.div whileTap={{ scale: 0.4 }}>
-          <LeftArrow iconClass="w-10 h-10 sm:w-14 sm:h-14 fill-gray-400 group-hover:fill-violet-700 opacity-50 group-hover:opacity-100" />
+          <LeftArrow iconClass="w-10 h-10 sm:w-14 sm:h-14 fill-gray-300 group-hover:fill-purple-800 opacity-60 group-hover:opacity-100" />
         </motion.div>
       </button>
     </div>
