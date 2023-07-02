e139b8b7354347fc63b9c1b544bd172aa3e7cf50
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-07-02 11:29:05 +0530

    add new places data

--- src/data/places.ts
+++ src/data/places.ts
@@ -15,9 +15,9 @@ const places: PlaceType[] = [
   },
   {
     id: "eca78d48-01a7-48ad-80b0-44ad2b316ac9",
-    name: "Ittoqqortoormiit",
-    country: "Greenland",
-    imageSrc: "Ittoqqortoormiit-Greenland.jpg",
+    name: "El Calafate, Santa Cruz",
+    country: "Argentina",
+    imageSrc: "El-Calafate-Santa-Cruz-Argentina.jpg",
   },
   {
     id: "845136e0-a0fe-42a8-a660-0683580fc6e7",
@@ -36,6 +36,78 @@ const places: PlaceType[] = [
     name: "Baie-Mahault, Guadeloupe",
     country: "France",
     imageSrc: "Baie-Mahault-Guadeloupe-France.jpg",
+  },
+  {
+    id: "656666f9-528e-4046-b66c-d42ff806dd18",
+    name: "Ban Gioc",
+    country: "Vietnam",
+    imageSrc: "Ban-Gioc-Vietnam.jpg",
+  },
+  {
+    id: "56ae3b22-359c-4871-b2b5-6794ff4e69f1",
+    name: "Banten",
+    country: "Indonesia",
+    imageSrc: "Banten-Indonesia.jpg",
+  },
+  {
+    id: "8b7c7032-cca5-484f-9b67-ced53cfbd4a6",
+    name: "Blyde River Canyon",
+    country: "South Africa",
+    imageSrc: "Blyde-River-Canyon-South-Africa.jpg",
+  },
+  {
+    id: "295c57c6-201a-4fea-a74b-52f046c7b8b9",
+    name: "Camalig, Bicol",
+    country: "Philippines",
+    imageSrc: "Camalig-Bicol-Philippines.jpg",
+  },
+  {
+    id: "f18509a6-4724-4433-ad5b-cf9165a7dbad",
+    name: "Dak-Ya, Gia-Lai",
+    country: "Vietnam",
+    imageSrc: "Dak_Ya-Gia_Lai-Vietnam.jpg",
+  },
+  {
+    id: "7728114e-e97b-451f-85c2-d2ddb0de87ae",
+    name: "Trentino South, Tyrol",
+    country: "Italy",
+    imageSrc: "Trentino-South-Tyrol-Italy.jpg",
+  },
+  {
+    id: "0a208bee-a024-4f37-9ee4-8c75dda5128b",
+    name: "Gramado",
+    country: "Brazil",
+    imageSrc: "Gramado-Brazil.jpg",
+  },
+  {
+    id: "edd89b68-2422-4488-b71a-720489cc6fb1",
+    name: "Kapa'a, Hawaii",
+    country: "United States",
+    imageSrc: "Kapa'a-Hawaii-United-States.jpg",
+  },
+  {
+    id: "c79f453c-d62c-4049-8daf-d57b65cd7ed9",
+    name: "Kecamatan Jombang",
+    country: "Indonesia",
+    imageSrc: "Kecamatan-Jombang-Indonesia.jpg",
+  },
+  {
+    id: "3a1a8733-38a5-48d9-ad1c-a596f83802c6",
+    name: "Kelingking Beach, Bali",
+    country: "Indonesia",
+    imageSrc: "Kelingking-Beach-Bali-Indonesia.jpg",
+  },
+  {
+    id: "4a7f1bc9-e35c-4d8d-aa70-aa0447641123",
+    name: "Krun",
+    country: "Germany",
+    imageSrc: "Krun-Germany.jpg",
+  },
+  {
+    id: "2ad488f4-3b3d-4472-9c2d-933d15555663",
+    name: "O Quy Ho Pass",
+    country: "Vietnam",
+    imageSrc: "O-Quy-Ho-pass-Vietnam.jpg",
   },
 ];
 
